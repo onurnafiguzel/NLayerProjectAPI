82cc637f796c8ab02bcf8816fda9395ff985290f
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-16 22:58:11 +0300

    Core katmanı tamamlandı.

---
+++ NLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
