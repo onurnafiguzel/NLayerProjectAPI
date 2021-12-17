bcabd8beece27b86556e86feb3880f2679c466b3
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-17 16:54:50 +0300

    UnitOfWork

---
+++ NLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data.Repositories;
+
+namespace NLayerProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public IProductRepository Products =>
+            _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories =>
+            _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
