b2b8be1d2959d227b33ce5655cdbc0081a868552
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-20 11:12:49 +0300

    ProductService

---
+++ NLayerProject.Service/Services/ProductService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Models;
+using NLayerProject.Core.Services;
+using NLayerProject.Core.UnitOfWorks;
+
+namespace NLayerProject.Service.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _unitOfWork.Products.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _unitOfWork.Products.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<Product>> Where(Expression<Func<Product, bool>> predicate)
+        {
+            return await _unitOfWork.Products.Where(predicate);
+        }
+
+        public async Task<Product> SingleOrDefaultAsync(Expression<Func<Product, bool>> predicate)
+        {
+            return await _unitOfWork.Products.SingleOrDefaultAsync(predicate);
+        }
+
+        public async Task<Product> AddAsync(Product entity)
+        {
+            await _unitOfWork.Products.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            await _unitOfWork.Products.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public void Remove(Product entity)
+        {
+            _unitOfWork.Products.Remove(entity);
+            _unitOfWork.Commit();
+        }
+
+        public void RemoveRange(IEnumerable<Product> entities)
+        {
+            _unitOfWork.Products.RemoveRange(entities);
+            _unitOfWork.Commit();
+        }
+
+        public Product Update(Product entity)
+        {
+            var updateProduct = _unitOfWork.Products.Update(entity);
+            _unitOfWork.Commit();
+            return updateProduct;
+        }
+
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
+        }
+    }
+}
