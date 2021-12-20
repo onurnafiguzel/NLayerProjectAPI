ce1a5be31fa95b11c16934785ff6b9a6f3884fbb
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-20 22:12:11 +0300

    AutoMapper

--- NLayerProject.API/Controllers/CategoryController.cs
+++ NLayerProject.API/Controllers/CategoryController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using NLayerProject.API.DTOs;
 using NLayerProject.Core.Services;
 
 namespace NLayerProject.API.Controllers
@@ -13,17 +15,19 @@ namespace NLayerProject.API.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var categories = await _categoryService.GetAllAsync();
-            return Ok(categories);
+            return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
         }
     }
 }
--- NLayerProject.API/Startup.cs
+++ NLayerProject.API/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using NLayerProject.Core.Repositories;
 using NLayerProject.Core.Services;
@@ -33,6 +34,7 @@ namespace NLayerProject.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped(typeof(IService<>), typeof(Service<>));
             services.AddScoped<ICategoryService, CategoryService>();
