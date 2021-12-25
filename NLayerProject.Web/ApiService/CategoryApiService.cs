bc3972ca9624664e3f8044dc87b26dfd69093842
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-25 13:31:49 +0300

    GetAllAsync

--- NLayerProject.Web/ApiService/CategoryApiService.cs
+++ NLayerProject.Web/ApiService/CategoryApiService.cs
@@ -1,4 +1,8 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NLayerProject.Web.DTOs;
 
 namespace NLayerProject.Web.ApiService
 {
@@ -9,6 +13,23 @@ namespace NLayerProject.Web.ApiService
         public CategoryApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetAllAsync()
+        {
+            IEnumerable<CategoryDto> categoryDtos;
+            var response = await _httpClient.GetAsync("categories");
+            if (response.IsSuccessStatusCode)
+            {
+                categoryDtos =
+                    JsonConvert.DeserializeObject<IEnumerable<CategoryDto>>(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                categoryDtos = null;
+            }
+
+            return categoryDtos;
         }
     }
 }
