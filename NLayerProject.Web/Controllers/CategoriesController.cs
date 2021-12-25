e2f605adeaf4857340b058b54890e472820f75c5
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-25 15:10:51 +0300

    Code refactoring

--- NLayerProject.Web/Controllers/CategoriesController.cs
+++ NLayerProject.Web/Controllers/CategoriesController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using NLayerProject.Core.Models;
-using NLayerProject.Core.Services;
 using NLayerProject.Web.ApiService;
 using NLayerProject.Web.DTOs;
 using NLayerProject.Web.Filters;
@@ -14,13 +10,11 @@ namespace NLayerProject.Web.Controllers
 {
     public class CategoriesController : Controller
     {
-        private readonly ICategoryService _categoryService;
         private readonly CategoryApiService _categoryApiService;
         private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService, IMapper mapper, CategoryApiService categoryApiService)
+        public CategoriesController(IMapper mapper, CategoryApiService categoryApiService)
         {
-            _categoryService = categoryService;
             _mapper = mapper;
             _categoryApiService = categoryApiService;
         }
